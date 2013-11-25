f7844908461fb4ccd78196bfff9c82255a0b6769
Adam Brewer <adamcbrewer@gmail.com>
2013-11-25 18:02:46 +0000

    Improving the 'when' questions

--- app/index.js
+++ app/index.js
@@ -27,18 +27,33 @@ LaunchpadGenerator.prototype.askFor = function askFor() {
     console.log('Launchpad is an opinionated HTML5 starter kit for quick web apps and sites.');
 
     var promptsFirst = [
+        // User/Developer
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Your name?',
+            default: null
+        },
+        {
+            type: 'input',
+            name: 'usertitle',
+            message: 'Your title?',
+            default: null
+        },
+        {
+            type: 'input',
+            name: 'usertwitter',
+            message: 'Your Twitter username?',
+            default: null
+        },
+        // Application
         {
             type: 'input',
             name: 'appname',
             message: 'What are you calling this thing?',
             default: 'MyApp'
         },
-        {
-            type: 'confirm',
-            name: 'humans',
-            message: 'Generate humans.txt?',
-            default: true
-        },
+        // Components & libraries
         {
             type: 'confirm',
             name: 'jquery',
@@ -51,65 +66,37 @@ LaunchpadGenerator.prototype.askFor = function askFor() {
             message: 'Include Modernizr?',
             default: false
         }
+
     ];
 
-    this.prompt(promptsFirst, function (props) {
+    this.prompt(promptsFirst, function (answers) {
 
-        this.data.appname = props.appname;
         self.data.bowerComponents = [];
 
-        var promptsSecond = [];
+        // Application
+        self.data.appname = answers.appname;
 
-        // Humans.txt related questions
-        if (props.humans) {
-            self.data.humans = {};
-
-            promptsSecond.push(
-                {
-                    type: 'input',
-                    name: 'username',
-                    message: 'Your name?',
-                    default: '<name>'
-                },
-                {
-                    type: 'input',
-                    name: 'usertitle',
-                    message: 'Your title?',
-                    default: '<title>'
-                },
-                {
-                    type: 'input',
-                    name: 'twittername',
-                    message: 'Your Twitter username?',
-                    default: '<twitter>'
-                }
-            );
-        }
-
-        // bower.json stuff
-        if (props.jquery) self.data.bowerComponents.push('"jquery": ""');
-        if (props.modernizr) self.data.bowerComponents.push('"modernizr": ""');
+        // Componenets & Libraries
+        if (answers.jquery) self.data.bowerComponents.push('"jquery": ""');
+        if (answers.modernizr) self.data.bowerComponents.push('"modernizr": ""');
 
         self.data.bowerComponents = self.data.bowerComponents.join(',');
 
-        this.prompt(promptsSecond, function (props) {
-            if (props.username) self.data.humans.username = props.username;
-            if (props.usertitle) self.data.humans.usertitle = props.usertitle;
-            if (props.twittername) self.data.humans.twittername = props.twittername;
+        self.data.humans = {};
+        self.data.humans.username = answers.username || '<name>';
+        self.data.humans.usertitle = answers.usertitle || '<title>';
+        self.data.humans.usertwitter = answers.usertwitter || '<twitter>';
 
-            cb();
-
-        });
+        cb();
 
     }.bind(this));
 };
 
 // Application important files here
 LaunchpadGenerator.prototype.app = function app() {
-    this.mkdir('app');
-    this.mkdir('app/templates');
+    // this.mkdir('app');
+    // this.mkdir('app/templates');
 
-    console.log(this.data.appname);
     this.template('_package.json', 'package.json');
     this.template('_bower.json', 'bower.json');
 };
@@ -117,7 +104,5 @@ LaunchpadGenerator.prototype.app = function app() {
 // The rest of the application files and directory structure
 LaunchpadGenerator.prototype.projectfiles = function projectfiles() {
     var data = this.data;
-    // console.log(data);
-    if (data.humans) this.template('humans.txt');
-    // this.copy('jshintrc', '.jshintrc');
+    this.template('humans.txt');
 };
